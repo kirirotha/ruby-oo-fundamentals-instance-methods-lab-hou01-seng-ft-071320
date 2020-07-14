b61a5dee5c1298ea38fbeb9765ae3d6c72307832
Kiri Um <kirirotha@gmail.com>
2020-07-14 13:20:28 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def barf
+    puts "Woof!"
+  end
+  def sit 
+    puts "The dog is sitting"
+  end
+end
+
+class Person
+  def talk
+    puts"Hello World!"
+  end
+  def walk
+    puts "The person is walking"
+  end
+end
